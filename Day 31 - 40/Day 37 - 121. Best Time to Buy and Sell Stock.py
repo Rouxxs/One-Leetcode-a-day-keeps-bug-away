851afd1fab257399935b04562a248cd6a00d79ec
Rou <88767858+Rouxxs@users.noreply.github.com>
2023-10-14 17:08:43 +0000

    Day 37

---
+++ Day 31 - 40/Day 37 - 121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        minPrice = prices[0]
+
+        for price in prices:
+            if price < minPrice:
+                minPrice = price
+            profit = max(profit, price - minPrice)
+
+        return profit
